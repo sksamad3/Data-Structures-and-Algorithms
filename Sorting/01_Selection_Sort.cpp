874e4b136c43c32ea9242487c3822ba9a4d6fe1d
Shaikh Samad <samadshaikh22005@gmail.com>
2025-03-13 23:27:57 +0530

    Learnt and Implemented Selection sort

--- Sorting/01_Selection_Sort.cpp
+++ Sorting/01_Selection_Sort.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void selection_Sort(int arr[] , int size){
-    for(int i = 0 ; i<size ; i++){
-        for(int j = i+1;  j<n ; j++){
-             
-            
+void selection_Sort(int arr[] , int n){
+    for(int i = 0 ; i< n-2 ; i++){
+         int mini = i ; 
+        for(int j = i+1 ; j<n ; j++){
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+            int temp = arr[i] ; 
+            arr[i] = arr[mini]; 
+            arr[mini] = temp; 
+
         }
     }
 }
@@ -21,5 +27,9 @@ int main(){
     }
 
     selection_Sort(arr , n); 
+    cout<<"Array : "; 
+    for(auto it : arr){
+        cout<<it<<" "; 
+    }
     return 0;
 }
